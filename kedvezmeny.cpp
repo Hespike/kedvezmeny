04209f244979ebd1e03a9df9469adbe6629bf813
Sárdi András <sardi.andras@gmail.com>
2021-10-19 14:06:17 +0200

    Create kedvezmeny.cpp

---
+++ kedvezmeny.cpp
@@ -0,0 +1,12 @@
+float bevasarlas(float ar){
+    float eredetiAr;
+    eredetiAr = ar * 0.9f;
+
+    return eredetiAr;
+}
+
+/*
+float bevasarlas(float eredeti) {
+    return eredeti * 0.9f;
+}
+ */
